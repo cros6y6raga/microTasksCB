929a73f487c57721edab9671475803c2fbf4234b
cros6y6raga <crosbybraga@yandex.ru>
2022-07-16 13:53:09 +0300

    commit1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Header} from './task1/Header';
+import {Body} from './task1/Body';
+import {Footer} from './task1/Footer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Header titleForHeader={'new header'}/>
+            <Body titleForBody={'new body'}/>
+            <Footer titleForFooter={'new footer'}/>
+        </>
+    );
 }
 
 export default App;
